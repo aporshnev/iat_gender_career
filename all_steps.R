af998a11ac94beff13e1357e5a5b769683beeab1
Alexander Porshnev <alexander.porshnev@mu.ie>
2025-02-24 14:54:44 +0000

    Step 3-5 update (folder structrue and path global variable)

--- all_steps.R
+++ all_steps.R
@@ -1,3 +1,18 @@
+#list of libraries used for steps 1-5 
+library(dplyr)
+library(knitr)
+library(psych)
+library(md.log)
+library(data.table)
+library(tidyr)
+library(lme4)
+library(flexmix)
+library(sjPlot)
+library(stringr)
+library(performance)
+library(ggplot2)
+library(cowplot)
+
 render_report = function(path, file_name) {
   rmarkdown::render(paste0(path,file_name, ".RmD"), params = list(path = path),
   output_file = paste0(path, file_name, ".html")
@@ -11,17 +26,17 @@ analysis_structure<-c(
   #          "/Step_01/01a_preprocessing",
   #          "/Step_01/01b_split_data",
   #          "/Step_02/02_mean_RT_for_stimuli_AB_sample_testing",
-            "/Step_02/02a_merge_RT",
+  #          "/Step_02/02a_\|merge_RT",
             "/Step_03/03_extend_RT",
             "/Step_04/04_LDM_models",
             "/Step_05/05_summary_report"
             )
-dir_structure<-c("Step_01/out_1_Clean/",
-                 "Step_01/out_1a_Clean_noexp/",
-                 "Step_01/out_1b_Split/",
-                 "Step_01/out_1a_Stats/",
-                 "Step_01/out_1_Stats/",
-                 "Step_02/out_2_RT/",
+dir_structure<-c(#"Step_01/out_1_Clean/",
+                 #"Step_01/out_1a_Clean_noexp/",
+                 #"Step_01/out_1b_Split/",
+                 #"Step_01/out_1a_Stats/",
+                 #"Step_01/out_1_Stats/",
+                 #"Step_02/out_2_RT/",
                  "Step_02/out_2a_RT_merged/",
                  "Step_03/out_3_EXT_RT/",
                  "Step_04/out_4_Models_classic/",
@@ -33,11 +48,14 @@ for (d in dir_structure){
 dir.create(file.path(path, d), showWarnings = FALSE)
 }
 
-render_report(path, a[1])
+#render_report(path, a[1])
+#file_name<-a[1]
+#sessionInfo()
+
 
 for (a in analysis_structure){
 render_report(path, a)}
 
-file_name<-a[1]
 
-sessionInfo()
+
+
