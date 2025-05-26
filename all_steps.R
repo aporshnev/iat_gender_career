2b43b8f683f5fbabdcc13f9113d028fc9460bda3
aporshnev <aporshnev@gmail.com>
2025-05-26 11:51:58 +0100

    V2 with nice Figures and dit moved to report and check for packages installed

--- all_steps.R
+++ all_steps.R
@@ -1,87 +1,77 @@
-#list of libraries used for steps 1-5 
-library(dplyr)
-library(knitr)
-library(psych)
-library(md.log)
-library(data.table)
-library(tidyr)
-library(lme4)
-library(flexmix)
-library(sjPlot)
-library(stringr)
-library(performance)
-library(ggplot2)
-library(cowplot)
+#List of all packages used on steps 1 - 5
+packages_setup<-c("dplyr",
+                  "knitr",
+                  "psych",
+                  "md.log",
+                  "data.table",
+                  "tidyr",
+                  "lme4",
+                  "flexmix",
+                  "sjPlot",
+                  "stringr",
+                  "performance",
+                  "ggplot2",
+                  "cowplot",
+                  "patchwork",
+                  "ggpubr") 
+
+#Check if packages are installed and install if needed
+for (p in packages_setup){
+  if (!requireNamespace(p)) {
+    install.packages(p)
+  }
+}
+
+#Constants for years of analysis
 
 start_year<-2005
 finish_year<-2021
 
-setwd("C:/Alex/Maynooth/IAT/Gender-Career/Analysis/git_for_replication/S3_5_full_check")
-path<-"C:/Alex/Maynooth/IAT/Gender-Career/Analysis/git_for_replication/S3_5_full_check"
+#setwd("C:/Alex/Maynooth/IAT/Gender-Career/Analysis/git_for_replication/S3_5_full_check")
+#path<-"C:/Alex/Maynooth/IAT/Gender-Career/Analysis/git_for_replication/S3_5_full_check"
 
+#Main report function
 render_report = function(path, file_name) {
   rmarkdown::render(paste0(path,file_name, ".RmD"), params = list(path = path, start_year, finish_year, model_type="classic"),
   output_file = paste0(path, file_name, "_",start_year,"-", finish_year,".html")
   )
 }
 
-
-
+#List of reports to create
 analysis_structure<-c(
 #  "/Step_01/01_preprocessing_year_data",
 #           "/Step_01/01a_preprocessing",
 #           "/Step_01/01b_split_data",
 #           "/Step_02/02_mean_RT_for_stimuli_AB_sample_testing",
-           "/Step_02/02a_merge_RT",
-           "/Step_03/03_extend_RT",
-  "/Step_04/04_LDM_models",
+#           "/Step_02/02a_merge_RT",
+#           "/Step_03/03_extend_RT",
+            "/Step_04/04_LDM_models",
             "/Step_05/05_summary_report"
             )
-dir_structure<-c("Step_01/out_1_Clean/",
-                 "Step_01/out_1a_Clean_noexp/",
-                 "Step_01/out_1b_Split/",
-                 "Step_01/out_1a_Stats/",
-                 "Step_01/out_1_Stats/",
-                 "Step_02/out_2_RT/",
-                 "Step_02/out_2a_RT_merged/",
-                 "Step_03/out_3_EXT_RT/",
-                 "Step_04/out_4_Models_classic/",
-                 "Step_04/out_4_Summary_classic/",
-                 "Step_05/out_05_Summary_Figures_classic/"
-                 )
 
-dir_structure<-c(
-  "Step_02/out_2a_RT_merged/",
-  "Step_03/out_3_EXT_RT/",
-  "Step_04/out_4_Models_classic/",
-  "Step_04/out_4_Summary_classic/",
-  "Step_05/out_05_Summary_Figures_classic/"
-)
 
-dir_structure<-paste0(dir_structure, start_year,"-", finish_year,"/")
-#creating directories structure 
-for (d in dir_structure){
- dir.create(file.path(path, d), showWarnings = FALSE)
-}
-
-#render_report(path, a[1])
-#file_name<-a[1]
-#sessionInfo()
+sessionInfo()
 
 #main pipeline
 for (a in analysis_structure){
 render_report(path, a)}
 
 
-years<-c(2009, 2014, 2019)
-for (y in years){
-start_year<-y
-finish_year<-y
-render_report(path, "/Step_05/05_summary_report")
-}
+#additional pipeline for robustness check & Figures, mean RT for one year only
+# years<-c(2009, 2014, 2019)
+# for (y in years){
+# start_year<-y
+# finish_year<-y 
+# render_report(path, "/Step_04/04_LDM_models")
+# render_report(path, "/Step_05/05_summary_report")
+# }
 
-start_year<-2005
-finish_year<-2021
-render_report(path, "/Step_05/05_summary_report")
+
+#additional pipeline for Figure smean RT for 2005-2021 years
+# start_year<-2005
+# finish_year<-2021
+# render_report(path, "/Step_04/04_LDM_models")
+# render_report(path, "/Step_05/05_summary_report")
+
 
 
